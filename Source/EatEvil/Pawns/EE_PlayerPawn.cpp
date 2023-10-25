b76164c39684ce9d22df3a703dc6865d92c7f143
Alexandr <miklester7@gmail.com>
2023-10-25 15:49:50 +0900

    CameraZoom

--- Source/EatEvil/Pawns/EE_PlayerPawn.cpp
+++ Source/EatEvil/Pawns/EE_PlayerPawn.cpp
@@ -4,6 +4,10 @@
 #include "Pawns/EE_PlayerPawn.h"
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetMathLibrary.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 DEFINE_LOG_CATEGORY_STATIC(AEE_PlayerPawnLog, All, All);
 
@@ -11,6 +15,15 @@ AEE_PlayerPawn::AEE_PlayerPawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
 	PrimaryActorTick.TickInterval = 0.0167f;
+
+	SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+	SetRootComponent(SceneComponent);
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SprintArmComponent"));
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 void AEE_PlayerPawn::BeginPlay()
@@ -19,6 +32,81 @@ void AEE_PlayerPawn::BeginPlay()
 
 	PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 	check(PlayerController);
+
+	if (PlayerController)
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void AEE_PlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AEE_PlayerPawn::Move);
+
+		EnhancedInputComponent->BindAction(ChangeZoomAction, ETriggerEvent::Started, this, &AEE_PlayerPawn::ZoomChange);
+	}
+}
+
+void AEE_PlayerPawn::Move(const FInputActionValue& Value)
+{
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void AEE_PlayerPawn::ZoomChange(const FInputActionValue& Value)
+{
+	const auto bIsIncrease = Value.Get<FInputActionValue::Axis1D>();
+	if (bIsIncrease == 0) return;
+
+	if (bIsIncrease > 0)
+	{
+		CurrentIncrementValue += IncrementValue;
+	}
+	else if(bIsIncrease < 0)
+	{
+		CurrentIncrementValue -= IncrementValue;
+	}
+	if (!GetWorldTimerManager().IsTimerActive(ZoomChangeTimer))
+	{
+		GetWorldTimerManager().SetTimer(ZoomChangeTimer, this, &AEE_PlayerPawn::UpdateZoom, FrequencyOfChange, true);
+	}
+}
+
+void AEE_PlayerPawn::UpdateZoom()
+{
+	if (CurrentIncrementValue > 0)
+	{
+		SpringArmComponent->TargetArmLength = FMath::Clamp(SpringArmComponent->TargetArmLength + IncrementStep, CameraMin, CameraMax);
+		CurrentIncrementValue -= IncrementStep;
+	}
+	else if (CurrentIncrementValue < 0)
+	{
+		SpringArmComponent->TargetArmLength = FMath::Clamp(SpringArmComponent->TargetArmLength - IncrementStep, CameraMin, CameraMax);
+		CurrentIncrementValue += IncrementStep;
+	}
+
+	if (CurrentIncrementValue == 0 || SpringArmComponent->TargetArmLength == CameraMin || SpringArmComponent->TargetArmLength == CameraMax)
+	{
+		CurrentIncrementValue = 0;
+		GetWorldTimerManager().ClearTimer(ZoomChangeTimer);
+	}
 }
 
 void AEE_PlayerPawn::Tick(float DeltaTime)
--- Source/EatEvil/Pawns/EE_PlayerPawn.h
+++ Source/EatEvil/Pawns/EE_PlayerPawn.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "EE_PlayerPawn.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class UCameraComponent;
+class USpringArmComponent;
 
 UCLASS()
 class EATEVIL_API AEE_PlayerPawn : public APawn
@@ -17,18 +23,60 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
-public:	
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
 	virtual void Tick(float DeltaTime) override;
 
+	void Move(const FInputActionValue& Value);
+	void ZoomChange(const FInputActionValue& Value);
 protected:
 	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Movement")
 	float MoveDistance{ 100.f };
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ChangeZoomAction;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USceneComponent* SceneComponent;
 
 private:
 	FVector2D GetViewportCenter();
 	FVector2D GetMouseViewportPosition();
 	FVector CursorDistFromCenter(FVector2D SubtractionResult, FVector2D Center);
 
+	void UpdateZoom();
+private:
 	UPROPERTY()
 	TObjectPtr<APlayerController> PlayerController;
+
+	FTimerHandle ZoomChangeTimer;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Zoom")
+	float CameraMin{ 300.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Zoom")
+	float CameraMax{ 1200.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Zoom")
+	float FrequencyOfChange{ 0.0167f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Zoom")
+	float IncrementValue{ 100.f };
+
+	UPROPERTY(EditDefaultsOnly, Category = "Zoom")
+	float IncrementStep{ 1.f };
+
+	float CurrentIncrementValue{ 0.f };
 };
